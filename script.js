0b794dd27e06383bc8cb0c9d465e252f6a2dc5c7
DanielAquinooo <danielaquinodeoliveira8@gmail.com>
2026-08-29 17:30:03 -0300

    Implement voice recognition on button click
    Added voice recognition functionality to the button click event.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const botao = document.getElementById("botao");
 
+const reconhecimento =
+    new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+reconhecimento.lang = "pt-BR";
+reconhecimento.continuous = false;
+reconhecimento.interimResults = false;
+
 botao.addEventListener("click", function () {
 
-    if ("webkitSpeechRecognition" in window) {
-
-        alert("RECONHECIMENTO DE VOZ DISPONÍVEL!");
-
-    } else if ("SpeechRecognition" in window) {
-
-        alert("RECONHECIMENTO DE VOZ DISPONÍVEL!");
-
-    } else {
-
-        alert("RECONHECIMENTO DE VOZ NÃO DISPONÍVEL!");
-
-    }
+    reconhecimento.start();
 
 });
+
+reconhecimento.onstart = function () {
+
+    console.log("🎤 Moto está ouvindo...");
+
+};
+
+reconhecimento.onresult = function (event) {
+
+    const texto = event.results[0][0].transcript;
+
+    console.log("Você disse:", texto);
+
+    alert("Moto ouviu: " + texto);
+
+};
+
+reconhecimento.onerror = function (event) {
+
+    console.log("Erro:", event.error);
+
+};
