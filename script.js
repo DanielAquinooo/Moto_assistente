015779fa8ded6856e0c5222fc8206ab980a078f6
DanielAquinooo <danielaquinodeoliveira8@gmail.com>
2026-08-29 17:23:43 -0300

    Add voice recognition functionality to button

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 const botao = document.getElementById("botao");
 
-botao.addEventListener("click", function () {
-    alert("BOTÃO FUNCIONANDO!");
-});
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (!SpeechRecognition) {
+
+    botao.addEventListener("click", function () {
+        alert("Seu navegador não suporta reconhecimento de voz.");
+    });
+
+} else {
+
+    const reconhecimento = new SpeechRecognition();
+
+    reconhecimento.lang = "pt-BR";
+    reconhecimento.continuous = false;
+    reconhecimento.interimResults = false;
+
+    botao.addEventListener("click", function () {
+
+        reconhecimento.start();
+
+    });
+
+    reconhecimento.onstart = function () {
+
+        console.log("🎤 Microfone ativado!");
+
+    };
+
+    reconhecimento.onresult = function (event) {
+
+        const texto = event.results[0][0].transcript;
+
+        console.log("Você disse:", texto);
+
+        alert("Você disse: " + texto);
+
+    };
+
+    reconhecimento.onerror = function (event) {
+
+        console.log("Erro:", event.error);
+
+        alert("Erro no microfone: " + event.error);
+
+    };
+}
