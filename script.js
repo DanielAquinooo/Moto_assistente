07fcd2cb5eb760ac50fe5da9fa1a6df577dfd4bd
DanielAquinooo <danielaquinodeoliveira8@gmail.com>
2026-08-30 10:23:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
 const botao = document.getElementById("botao");
 
-const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-
-if (!SpeechRecognition){
-    alert("Seu navegador não suporta reconhecimento de voz.");
-} else {
-    
-    const reconhecimento = new SpeechRecognition();
-    
-reconhecimento.lang = "pt-BR";
-reconhecimento.continuous = false;
-reconhecimento.interimResults = false;
-
-    botao.addEventListener("click",  function (){
-
-            console.log("Botão presionado!");
-
-        reconhecimento.start();
-
-    });
-
-    reconhecimento.onstart = function (){
-        console.log("Baymax está ouvindo!");
-
-    reconhecimento.onresult = function (event) {
-        const texto = event.results[0][0].transcript;
-        console.log("Você disse:", texto);
-        alert("Você disse:" + texto);
-};
-
-        reconhecimento.onerror - function (event){
-            console.log("Microfone encerrado.");
-
-        };
-    }
+console.log("BOTÃO ENCONTRADO:", botao);
