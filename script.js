960620fcb1cfe8f8362cef6b98f19fec3f30a08a
DanielAquinooo <danielaquinodeoliveira8@gmail.com>
2026-08-29 21:27:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,126 @@
 const botao = document.getElementById("botao");
 
 const reconhecimento =
-    new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 
 reconhecimento.lang = "pt-BR";
 reconhecimento.continuous = false;
 reconhecimento.interimResults = false;
 
+// 🔊 Função para o Moto falar
+function falar(texto) {
+
+```
+const mensagem = new SpeechSynthesisUtterance(texto);
+
+mensagem.lang = "pt-BR";
+mensagem.rate = 1;
+mensagem.pitch = 1;
+
+window.speechSynthesis.cancel();
+window.speechSynthesis.speak(mensagem);
+```
+
+}
+
+// 🎤 Botão
 botao.addEventListener("click", function () {
 
-    reconhecimento.start();
+```
+reconhecimento.start();
+```
 
 });
 
+// 🎤 Começou a ouvir
 reconhecimento.onstart = function () {
 
-    console.log("🎤 Moto está ouvindo...");
+```
+console.log("Baymax está ouvindo...");
+```
 
 };
 
+//Cérebro básico
 reconhecimento.onresult = function (event) {
 
-    const texto = event.results[0][0].transcript;
+```
+const texto = event.results[0][0].transcript.toLowerCase();
 
-    console.log("Você disse:", texto);
+console.log("Você disse:", texto);
 
-    alert("Moto ouviu: " + texto);
+responder(texto);
+```
 
 };
 
+//Cérebro
+function responder(texto) {
+
+```
+if (texto.includes("olá") || texto.includes("oi")) {
+
+    falar("Olá! Como posso ajudar?");
+
+}
+
+else if (texto.includes("seu nome")) {
+
+    falar("Meu nome é Baymax.");
+
+}
+
+else if (texto.includes("quem é você")) {
+
+    falar("Eu sou o Baymax, seu assistente virtual.");
+
+}
+
+else if (texto.includes("tudo bem"))
+else if (texto.includes("tudo bemm, Baymax?")){
+
+    falar("Tudo ótimo! E com você?");
+
+}
+
+else if (
+    texto.includes("obrigado") ||
+    texto.includes("obrigada")
+) {
+
+    falar("Por nada!");
+
+}
+
+else if (texto.includes("bom dia"))
+else if (texto.includes("bom dia, Baymax")){
+
+    falar("Bom dia! Espero que seu dia seja ótimo.");
+
+}
+
+else if (texto.includes("boa noite"))
+else if (texto.includes("boa noite, Baymax")){
+
+    falar("Boa noite! Até mais.");
+
+}
+
+else {
+
+    falar("Ainda não sei responder isso.");
+
+}
+```
+
+}
+
+// ⚠️ Erros
 reconhecimento.onerror = function (event) {
 
-    console.log("Erro:", event.error);
+```
+console.log("Erro:", event.error);
+```
 
 };
+
