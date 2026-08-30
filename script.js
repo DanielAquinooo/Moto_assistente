b5f888948260740b8faf3a277f6b32fbf3c33885
DanielAquinooo <danielaquinodeoliveira8@gmail.com>
2026-08-30 10:16:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,37 @@
 const botao = document.getElementById("botao");
 
-const reconhecimento =
-new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 
+if (!SpeechRecognition){
+    alert("Seu navegador não suporta reconhecimento de voz.");
+} else {
+    
+    const reconhecimento = new SpeechRecognition();
+    
 reconhecimento.lang = "pt-BR";
 reconhecimento.continuous = false;
 reconhecimento.interimResults = false;
 
-// Falar
-function falar(texto) {
+    botao.addEventListenner("click",  function (){
 
-```
-const mensagem = new SpeechSynthesisUtterance(texto);
+            console.log("Botão presionado!");
 
-mensagem.lang = "pt-BR";
-mensagem.rate = 1;
-mensagem.pitch = 1;
+        reconhecimento.start();
 
-window.speechSynthesis.cancel();
-window.speechSynthesis.speak(mensagem);
-```
+    });
 
-}
+    reconhecimento.onstart = function (){
+        console.log("Baymax está ouvindo!");
 
-// Botão
-botao.addEventListener("click", function () {
-    console.log("Botão presionado")
-reconhecimento.start();
-});
-
-// Ouvir
-reconhecimento.onstart = function () {
-
-```
-console.log("Baymax está ouvindo...");
-```
-
+    reconhecimento.onresult = function (event) {
+        const texto = event.results[0][0].transcript;
+        console.log("Você disse:", texto);
+        alert("Você disse:" + texto);
 };
 
-//Cérebro básico
-reconhecimento.onresult = function (event) {
+        reconhecimento.onerror - function (event){
+            console.log("Microfone encerrado.");
 
-```
-const texto = event.results[0][0].transcript.toLowerCase();
-
-console.log("Você disse:", texto);
-
-responder(texto);
-```
-
-};
-
-//Cérebro
-function responder(texto) {
-
-```
-if (texto.includes("olá") || texto.includes("oi")) {
-
-    falar("Olá! Como posso ajudar?");
-
-}
-
-else if (texto.includes("seu nome")) {
-
-    falar("Meu nome é Baymax.");
-
-}
-
-else if (texto.includes("quem é você")) {
-
-    falar("Eu sou o Baymax, seu assistente virtual.");
-
-}
-
-else if (texto.includes("tudo bem"))
-else if (texto.includes("tudo bemm, Baymax?")){
-
-    falar("Tudo ótimo! E com você?");
-
-}
-
-else if (
-    texto.includes("obrigado") ||
-    texto.includes("obrigada")
-) {
-
-    falar("Por nada!");
-
-}
-
-else if (texto.includes("bom dia"))
-else if (texto.includes("bom dia, Baymax")){
-
-    falar("Bom dia! Espero que seu dia seja ótimo.");
-
-}
-
-else if (texto.includes("boa noite"))
-else if (texto.includes("boa noite, Baymax")){
-
-    falar("Boa noite! Até mais.");
-
-}
-
-else {
-
-    falar("Ainda não sei responder isso.");
-
-}
-```
-
-}
-
-// ⚠️ Erros
-reconhecimento.onerror = function (event) {
-
-```
-console.log("Erro:", event.error);
-```
-
-};
-
+        };
+    }
